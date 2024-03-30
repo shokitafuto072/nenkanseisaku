43bbdcae883a72d6a4d58206b0c5516bd26dc581
futoShokita <fko2347090@stu.o-hara.ac.jp>
2024-03-30 20:58:32 +0900

    add file

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class BaseView(TemplateView):
+    template_name='base.html'
+
+
+
+
+
+# Create your views here.
+
+
